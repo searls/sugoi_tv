4ad93c5a300e738d776fcb3b8a7bbf74ef552010
Justin Searls <justin@searls.co>
2026-02-10 15:50:35 -0500

    Initial Commit

---
+++ SugoiTV/SugoiTVApp.swift
@@ -0,0 +1,32 @@
+//
+//  SugoiTVApp.swift
+//  SugoiTV
+//
+//  Created by Justin Searls on 2/10/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SugoiTVApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SugoiTVTests/SugoiTVTests.swift
@@ -0,0 +1,16 @@
+//
+//  SugoiTVTests.swift
+//  SugoiTVTests
+//
+//  Created by Justin Searls on 2/10/26.
+//
+
+import Testing
+
+struct SugoiTVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
